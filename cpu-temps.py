ae6fdf3cdf4e2f4b4d74c2acdd2273bbfdfb94e6
kaypineda <kpine003@odu.edu>
2022-10-20 15:07:13 -0400

    added read and print functions

--- cpu-temps.py
+++ cpu-temps.py
@@ -2,11 +2,54 @@
 Kayla Pineda   UIN: 01168338 '''
 
 import argparse
+import parse_temps as pt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file', help='input data file')
 args = parser.parse_args()
 
+
+def read_file(input_file):
+    # Following code adapted from Prof Kennedy
+    temp_file = open(input_file, 'r')
+    times = []
+    core0 = []
+    core1 = []
+    core2 = []
+    core3 = []
+    for time, core_data in pt.parse_raw_temps(temp_file):
+        times.append(time)
+        core0.append(core_data[0])
+        core1.append(core_data[1])
+        core2.append(core_data[2])
+        core3.append(core_data[3])
+
+    temp_file.close()
+
+    return times, core0, core1, core2, core3
+
+
+def print_all_cores(times, core0, core1, core2, core3):
+    for i,t in enumerate(times):
+        print(f'{times[i]:<7} || {core0[i]:<5} | {core1[i]:<5} | {core2[i]:<5} | {core3[i]:<5} |')
+
+
+def piecewise_linear_interpolation(time, data):
+    print("Not yet implemented")
+    # take the slope between two points
+    ## for eve i and i + 1 in data
+    ## (data[i + 1] - data[i]) / (time[i + 1] - time[i])
+
+
+def least_squares_approximation(time, data):
+    print("Not yet implemented")
+
+
 if __name__ == "__main__":
     print(args.input_file)
+
+    times, core0, core1, core2, core3 = read_file(args.input_file)
+    print_all_cores(times, core0, core1, core2, core3)
+
+
 
