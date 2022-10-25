eb2469d7d5439115d5fadf01ec14e961b0dfc02f
kaypineda <kpine003@odu.edu>
2022-10-25 11:52:09 -0400

    updated write func, added documentation

--- utils.py
+++ utils.py
@@ -5,8 +5,15 @@ from dateutil import parser
 
 
 def read_file(input_file):
-    # Following code adapted from Prof Kennedy
+    '''
+    Read data from an input file and store, code adapted from Prof Kennedy's parse_temps example
+    :param input_file: the input file
+
+    :return: array containing the times and an array containing cpu temsps for each core
+    '''
+
     temp_file = open(input_file, 'r')
+
     times = []
     core0 = []
     core1 = []
@@ -25,6 +32,12 @@ def read_file(input_file):
 
 
 def get_date(input_file):
+    '''
+    Extract the date from an input file name
+    :param input_file: the input file
+    :return: the date from the input file
+    '''
+
     full_date = parser.parse(input_file, fuzzy=True)
     date = str(full_date)[:10]
 
@@ -32,17 +45,43 @@ def get_date(input_file):
 
 
 def print_all_cores(times, core0, core1, core2, core3):
+    '''
+    Print the data of all cores
+    :param times: times in seconds
+    :param core0: cpu temps of core 0
+    :param core1: cpu temps of core 1
+    :param core2: cpu temps of core 2
+    :param core3: cpu temps of core 3
+
+    :return: prints all data
+    '''
     for i,t in enumerate(times):
         print(f'{times[i]:<7} || {core0[i]:<5} | {core1[i]:<5} | {core2[i]:<5} | {core3[i]:<5} |')
 
 
 def print_single_core(times, core):
+    '''
+    Prints data of single core
+    :param times: times in seconds
+    :param core: cpu temps of a core
+
+    :return: prints core data
+    '''
     for i,t in enumerate(times):
         print(f'{times[i]:<7} || {core[i]:<5} |')
 
 
-def create_output_file(date, core, times, data, type):
-    output_file = "output/" + date + "-" + type + "-" + core + ".txt"
+def write_output_file(date, core, times, data):
+    """
+    Creates output file
+    :param date: the date the data was acquired
+    :param core: name of the core
+    :param times: times in seconds
+    :param data: piecewise linear interpolation of a core
+
+    :return: output file listing piecewise linear interpolation data of a core
+    """
+    output_file = "output/" + date + "-" + core + ".txt"
     output = open(output_file, "w")
 
     for i,t in enumerate(times):
@@ -52,7 +91,7 @@ def create_output_file(date, core, times, data, type):
         y_int, m = data[i]
         data_rounded = '{:.4f}'.format(round(m, 4))
 
-        output.write(f'{times[i]:<7} <= x < {times[i+1]:>7}; y_{i:<5} = {y_int:>5} + {data_rounded:>7}x; {type} \n')
-        # print(f'{times[i]:<6} <= x < {times[i + 1]:>6}; y_{i:<5} = {y_int:<5} + {data_rounded:>7}x; {type}')
+        output.write(f'{times[i]:<7} <= x < {times[i+1]:>7}; y_{i:<5} = {y_int:>5} + {data_rounded:>7}x; '
+                     f'interpolation \n')
 
     output.close()
